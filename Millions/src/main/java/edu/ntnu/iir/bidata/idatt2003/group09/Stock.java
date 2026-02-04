ea2b9f960847476bb68c2bf3e576b2040a602d48
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-04 12:27:33 +0100

    finished the stock class

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/Stock.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/Stock.java
@@ -1,7 +1,47 @@
 package edu.ntnu.iir.bidata.idatt2003.group09;
 
-package Stock{
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.ArrayList;
 
-private String symbol;private String company;private List<BigDecimal>prices;
+public class Stock {
 
-public Stock(String symbol,String company)}
+  private String symbol;
+  private String company;
+  private List<BigDecimal> prices;
+
+  public Stock(String symbol, String company, BigDecimal salesPrice) {
+    this.symbol = symbol;
+    this.company = company;
+    prices = new ArrayList<>();
+    prices.add(salesPrice);
+  }
+
+  /***
+   * Getters
+   */
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  /***
+   * returns the latest price from the prices
+   * list
+   */
+  public BigDecimal getSalesPrice() {
+    return prices.getLast();
+  }
+
+  /***
+   * Add the argumetn price to the last index
+   * in the prices list
+   */
+  public void addNewSalesPrice(BigDecimal price) {
+    prices.add(price);
+  }
+
+}
