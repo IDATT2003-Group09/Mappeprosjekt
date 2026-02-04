e7165951d5202ad1f8308353aceac682815c4c91
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-04 12:13:02 +0100

    first commit

---
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/Stock.java
@@ -0,0 +1,7 @@
+package edu.ntnu.iir.bidata.idatt2003.group09;
+
+package Stock{
+
+private String symbol;private String company;private List<BigDecimal>prices;
+
+public Stock(String symbol,String company)}
