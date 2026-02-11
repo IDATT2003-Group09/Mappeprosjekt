32dbcdc680f256596b759b82e1f0dbca2688ecc7
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-11 13:24:01 +0100

    started class missing where to get salesprice

---
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/SaleCalculator.java
@@ -0,0 +1,40 @@
+package edu.ntnu.iir.bidata.idatt2003.group09;
+
+import java.math.BigDecimal;
+
+public class SaleCalculator implements TransactionCalculator {
+
+  private BigDecimal purchasePrice;
+  private BigDecimal salesPrice;
+  private BigDecimal quantity;
+
+  public SaleCalculator(Share share) {
+    this.purchasePrice = share.getPurchasePrice();
+    this.salesPrice = // missing
+        this.quantity = share.getQuantity();
+  }
+
+  @Override
+  public BigDecimal calculateGross() {
+    return salesPrice.multiply(quantity);
+  }
+
+  @Override
+  public BigDecimal calculateCommission() {
+    return calculateGross().multiply(new BigDecimal(0.01));
+  }
+
+  @Override
+  public BigDecimal calculateTax() {
+    BigDecimal originalPrice = purchasePrice.multiply(quantity);
+    BigDecimal taxRate = new BigDecimal(0.3);
+    return calculateGross().subtract(originalPrice).subtract(calculateCommission()).multiply(taxRate);
+  }
+
+  @Override
+  public BigDecimal calculateTotal() {
+    BigDecimal originalPrice = purchasePrice.multiply(quantity);
+    BigDecimal earnings = calculateGross().subtract(originalPrice);
+    return earnings.subtract(calculateCommission()).subtract(calculateTax());
+  }
+}
