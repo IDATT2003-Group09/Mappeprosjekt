2296a10ded1ba245bbb92e944f26867ee29de990
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-18 13:25:17 +0100

    made all tests

---
+++ Millions/src/test/java/edu/ntnu/iir/bidata/idatt2003/group09/PurchaseCalculatorTest.java
@@ -0,0 +1,40 @@
+package edu.ntnu.iir.bidata.idatt2003.group09;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PurchaseCalculatorTest {
+
+  private PurchaseCalculator calculator;
+
+  @BeforeEach
+  void setUp() {
+    Stock stock = new Stock("AAPL", "Apple", new BigDecimal("150.00"));
+    Share share = new Share(stock, new BigDecimal("2.5"), new BigDecimal("10.00"));
+    calculator = new PurchaseCalculator(share);
+  }
+
+  @Test
+  void calculateGrossShouldReturnPriceTimesQuantity() {
+    assertEquals(new BigDecimal("25.000"), calculator.calculateGross());
+  }
+
+  @Test
+  void calculateCommissionShouldReturnFivePercentOfGross() {
+    assertEquals(new BigDecimal("1.25000"), calculator.calculateCommission());
+  }
+
+  @Test
+  void calculateTaxShouldReturnZero() {
+    assertEquals(new BigDecimal("0"), calculator.calculateTax());
+  }
+
+  @Test
+  void calculateTotalShouldReturnGrossPlusCommission() {
+    assertEquals(new BigDecimal("26.25000"), calculator.calculateTotal());
+  }
+}
