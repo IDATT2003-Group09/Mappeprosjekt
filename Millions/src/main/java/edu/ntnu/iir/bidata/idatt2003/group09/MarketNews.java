e1b691eeaacf183fe760b5a0a77bba7558bafdf7
davidomm <davidomm@stud.ntnu.no>
2026-03-25 12:20:23 +0100

    small change to be able to make news headlines

---
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/MarketNews.java
@@ -0,0 +1,35 @@
+package edu.ntnu.iir.bidata.idatt2003.group09;
+
+import java.math.BigDecimal;
+
+public class MarketNews {
+
+    private final String headline;
+    private final String description;
+
+    private final String sector;
+    private final BigDecimal impact;
+
+    public MarketNews(String headline, String description, String sector, BigDecimal impact) {
+        this.headline = headline;
+        this.description = description;
+        this.sector = sector;
+        this.impact = impact;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public BigDecimal getImpact() {
+        return impact;
+    }
+}
