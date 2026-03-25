132e64d9c68e678149acf501600acfd2e33f2429
davidomm <davidomm@stud.ntnu.no>
2026-03-25 12:04:48 +0100

    class for market event with getters

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/MarketEvent.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/MarketEvent.java
@@ -1,5 +1,28 @@
 package edu.ntnu.iir.bidata.idatt2003.group09;
 
+import java.math.BigDecimal;
+
 public class MarketEvent {
 
+    private final String name;
+    private final String sector;
+    private final BigDecimal impact;
+
+    public MarketEvent(String name, String sector, BigDecimal impact) {
+        this.name = name;
+        this.sector = sector;
+        this.impact = impact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public BigDecimal getImpact() {
+        return impact;
+    }
 }
