c1a0b501145f78330622f3fbaa29bbb2967609dd
davidomm <davidomm@stud.ntnu.no>
2026-03-31 21:18:50 +0200

    methods for stocks

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/controller/GameController.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/controller/GameController.java
@@ -3,6 +3,7 @@ package edu.ntnu.iir.bidata.idatt2003.group09.controller;
 import edu.ntnu.iir.bidata.idatt2003.group09.base.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 //Extend this class anywhere if needed for more game logic,
 //maybe split into multiple classes later if needed
@@ -31,6 +32,26 @@ public class GameController {
         return player.getNetWorth();
     }
 
-    //get title metode
+    public PlayerStatus getStatus() {
+        return player.getStatus(exchange.getWeek());
+    }
+
+    //stock info
+    public List<Stock> getAllStocks() {
+        return exchange.getStockMap().values().stream().toList();
+    }
+
+    public Stock getStock(String symbol) {
+        return exchange.getStock(symbol);
+    }
+
+    public List<Stock> getGainers() {
+        return exchange.getGainers();
+    }
+
+    public List<Stock> getLosers() {
+        return exchange.getLosers();
+    }
+
 
 }
