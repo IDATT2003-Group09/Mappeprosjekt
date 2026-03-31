06a64e33de46befa16751bc0ff31a98b3e7ab28e
davidomm <davidomm@stud.ntnu.no>
2026-03-31 20:43:26 +0200

    finished up the enum class.

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/PlayerStatus.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/PlayerStatus.java
@@ -1,7 +1,17 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.base;
 
 public enum PlayerStatus {
-    NOVICE,
-    INVESTOR,
-    SPECULATOR
+    NOVICE("Novice"),
+    INVESTOR("Investor"),
+    SPECULATOR("Speculator");
+
+    private final String displayName;
+
+    PlayerStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
