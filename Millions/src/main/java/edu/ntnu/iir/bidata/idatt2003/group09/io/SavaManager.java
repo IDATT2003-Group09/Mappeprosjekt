7cfe322018f724f1fb0b92f08991add0048ed20e
davidomm <davidomm@stud.ntnu.no>
2026-04-06 16:35:58 +0200

    added the method to save a file

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/io/SavaManager.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/io/SavaManager.java
@@ -1,4 +1,18 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.io;
 
+import java.io.*;
+
 public class SavaManager {
+
+    private static final String FILE = "savegame.dat";
+
+    public static void save(GameState state) {
+        try (ObjectOutputStream out =
+                new ObjectOutputStream(new FileOutputStream(FILE))) {
+            out.writeObject(state);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
