de824f32023e6123fa05425dbcee384b91058af5
davidomm <davidomm@stud.ntnu.no>
2026-04-06 14:38:13 +0200

    basic startscreen with three options. Using a handler for these options.

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/StartScreen.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/StartScreen.java
@@ -1,4 +1,35 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.ui.screen;
 
-public class StartScreen {
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class StartScreen extends VBox {
+
+    public interface StartHandler {
+        void onNewGame();
+        void onLoadGame();
+        void onSettings();
+    }
+
+    public StartScreen(StartHandler handler) {
+
+        setSpacing(20);
+        setAlignment(Pos.CENTER);
+        setStyle("-fx-background-color: Grey;");
+
+        Button newGameBtn = new Button("Nytt Spill");
+        Button loadGameBtn = new Button("Last Spill");
+        Button settingsBtn = new Button("Innstillinger");
+
+        newGameBtn.setPrefWidth(400);
+        loadGameBtn.setPrefWidth(400);
+        settingsBtn.setPrefWidth(400);
+
+        newGameBtn.setOnAction(e -> handler.onNewGame());
+        loadGameBtn.setOnAction(e -> handler.onLoadGame());
+        settingsBtn.setOnAction(e -> handler.onSettings());
+
+        getChildren().addAll(newGameBtn, loadGameBtn, settingsBtn);
+    }
 }
