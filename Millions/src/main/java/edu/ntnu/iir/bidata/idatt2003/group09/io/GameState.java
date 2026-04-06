844a59f008e5d53dd89d6a94ee0e7b084d991c66
davidomm <davidomm@stud.ntnu.no>
2026-04-06 16:29:20 +0200

    klassen henter player og exchange. lager en gamestate fra dette, gamestate skal brukes for videre for å kunne lagre data

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/io/GameState.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/io/GameState.java
@@ -1,4 +1,25 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.io;
 
-public class GameState {
+import edu.ntnu.iir.bidata.idatt2003.group09.base.Exchange;
+import edu.ntnu.iir.bidata.idatt2003.group09.base.Player;
+
+import java.io.Serializable;
+
+public class GameState implements Serializable {
+
+    private final Player player;
+    private final Exchange exchange;
+
+    public GameState(Player player, Exchange exchange) {
+        this.player = player;
+        this.exchange = exchange;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Exchange getExchange() {
+        return exchange;
+    }
 }
