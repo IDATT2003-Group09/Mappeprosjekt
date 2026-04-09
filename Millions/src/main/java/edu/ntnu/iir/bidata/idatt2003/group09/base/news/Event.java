28feb0412bcbd511f41bd1f0c6d287d7b3e34a93
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-09 21:38:31 +0200

    started event implementation

---
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/news/Event.java
@@ -0,0 +1,13 @@
+package edu.ntnu.iir.bidata.idatt2003.group09.base.news;
+
+import java.util.HashMap;
+import java.math.BigDecimal;
+
+public class Event {
+  
+  private HashMap<String, BigDecimal> eventData;
+
+  public Event(){
+
+  }
+}
