3bb2f40b7efdc49de33a69efbe7c5a5c6ffee058
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-09 20:24:38 +0200

    framework

---
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/io/EnhanceCSV.java
@@ -0,0 +1,16 @@
+package edu.ntnu.iir.bidata.idatt2003.group09.io;
+
+public class EnhanceCSV {
+
+  private String filePath;
+  
+  public EnhanceCSV(String filePath) {
+    this.filePath = filePath;
+  }
+  
+  private void addTag(String tag) {
+
+  }
+
+
+}
