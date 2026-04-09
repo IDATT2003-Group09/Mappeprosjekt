1c62650b0fa90d84e0496826947148a9d73bec02
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-09 21:57:19 +0200

    better start

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/news/StockSpecificEvent.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/news/StockSpecificEvent.java
@@ -7,9 +7,20 @@ import java.util.Map;
 
 public class StockSpecificEvent {
 
+  private String partialHeadline;
+  private String partialDescription;
 
 	public StockSpecificEvent(String partialHeadline, String partialDescription) {
+		this.partialHeadline = partialHeadline;
+		this.partialDescription = partialDescription;
 	}
 
+  public String generateFullHeadline(String stockName) {
+    return partialHeadline.replace("{stock}", stockName);
+  }
+
+  public String generateFullDescription(String stockName) {
+    return partialDescription.replace("{stock}", stockName);
+  }
 
 }
