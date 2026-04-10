bce8e73e279d7014f9a1240e7807636392a937cb
davidomm <davidomm@stud.ntnu.no>
2026-04-10 12:46:13 +0200

    added more methods

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/Game/GameProgress.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/Game/GameProgress.java
@@ -1,11 +1,14 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.base.Game;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class GameProgress {
 
     private int currentLevel = 1;
     private final BigDecimal baseRequirement;
+    private int currentWeek = 0;
+    private final int weeksPerQuarter = 13;
 
     public GameProgress(BigDecimal baseRequirement) {
         this.baseRequirement = baseRequirement;
@@ -21,5 +24,25 @@ public class GameProgress {
 
     public int getCurrentLevelNumber() {
         return currentLevel;
+    }
+
+    public void nextWeek() {
+        currentWeek++;
+    }
+
+    public boolean isQuarterComplete() {
+        return currentWeek % weeksPerQuarter == 0;
+    }
+
+    public int getCurrentWeek() {
+        return currentWeek;
+    }
+
+    public boolean meetsRequirement(BigDecimal netWorth, BigDecimal startingMoney) {
+
+        BigDecimal growth = netWorth.subtract(startingMoney)
+                .divide(startingMoney, 4, RoundingMode.HALF_UP);
+
+        return growth.compareTo(getCurrentLevel().getRequiredGrowth()) >= 0;
     }
 }
