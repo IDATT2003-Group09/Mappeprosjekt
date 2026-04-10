63093361f780777106c26fbb01911eb1ac52c511
davidomm <davidomm@stud.ntnu.no>
2026-04-10 10:49:27 +0200

    build table method with values for week, type, ticker, quantity, price, total

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/TransactionHistoryScreen.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/TransactionHistoryScreen.java
@@ -2,6 +2,9 @@ package edu.ntnu.iir.bidata.idatt2003.group09.ui.screen;
 
 import edu.ntnu.iir.bidata.idatt2003.group09.base.transaction.Transaction;
 import edu.ntnu.iir.bidata.idatt2003.group09.controller.GameController;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
@@ -24,5 +27,80 @@ public class TransactionHistoryScreen extends BorderPane {
         refresh();
 
         setCenter(table);
+    }
+
+    private void buildTable() {
+
+        TableColumn<Transaction, Integer> weekCol = new TableColumn<>("Week");
+        weekCol.setCellValueFactory(data ->
+                new javafx.beans.property.SimpleIntegerProperty(data.getValue().getWeek()).asObject()
+        );
+
+        TableColumn<Transaction, String> typeCol = new TableColumn<>("Type");
+        typeCol.setCellValueFactory(data ->
+                new javafx.beans.property.SimpleStringProperty(
+                        data.getValue().getClass().getSimpleName().replace("Purchase", "Buy").replace("Sale", "Sell")
+                )
+        );
+
+        typeCol.setCellFactory(col -> new TableCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                    setStyle("");
+                    return;
+                }
+
+                setText(item);
+
+                if (item.equals("Buy")) {
+                    setStyle("-fx-text-fill: green;");
+                } else {
+                    setStyle("-fx-text-fill: red;");
+                }
+            }
+        });
+
+        TableColumn<Transaction, String> tickerCol = new TableColumn<>("Ticker");
+        tickerCol.setCellValueFactory(data ->
+                new javafx.beans.property.SimpleStringProperty(
+                        data.getValue().getShare().getStock().getSymbol()
+                )
+        );
+
+        TableColumn<Transaction, String> qtyCol = new TableColumn<>("Qty");
+        qtyCol.setCellValueFactory(data ->
+                new javafx.beans.property.SimpleStringProperty(
+                        data.getValue().getShare().getQuantity().toString()
+                )
+        );
+
+        TableColumn<Transaction, String> priceCol = new TableColumn<>("Price");
+        priceCol.setCellValueFactory(data -> {
+            var share = data.getValue().getShare();
+
+            return new javafx.beans.property.SimpleStringProperty(
+                    currencyFormat.format(share.getPurchasePrice())
+            );
+        });
+
+        TableColumn<Transaction, String> totalCol = new TableColumn<>("Total");
+        totalCol.setCellValueFactory(data ->
+                new javafx.beans.property.SimpleStringProperty(
+                        currencyFormat.format(
+                                data.getValue().getCalculator().calculateTotal()
+                        )
+                )
+        );
+
+        table.getColumns().addAll(weekCol, typeCol, tickerCol, qtyCol, priceCol, totalCol);
+    }
+
+    public void refresh() {
+        table.setItems(FXCollections.observableArrayList(
+                controller.getPlayer().getTransactionArchive().getAllTransactions()
+        ));
     }
 }
