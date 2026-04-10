930c450630aa5157bfe2c5cbd8d8e9170141165c
davidomm <davidomm@stud.ntnu.no>
2026-04-10 10:02:15 +0200

    constructor for the transaction history screen

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/TransactionHistoryScreen.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/TransactionHistoryScreen.java
@@ -1,4 +1,28 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.ui.screen;
 
-public class TransactionHistoryScreen {
+import edu.ntnu.iir.bidata.idatt2003.group09.base.transaction.Transaction;
+import edu.ntnu.iir.bidata.idatt2003.group09.controller.GameController;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class TransactionHistoryScreen extends BorderPane {
+
+    private final GameController controller;
+    private final TableView<Transaction> table;
+    private final NumberFormat currencyFormat;
+
+    public TransactionHistoryScreen(GameController controller) {
+        this.controller = controller;
+        this.currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+
+        table = new TableView<>();
+
+        buildTable();
+        refresh();
+
+        setCenter(table);
+    }
 }
