0dcee907672144ecd2d9212ece70e6284162ae5a
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-10 15:05:33 +0200

    boss is create newgame screen

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/CreateGameScreen.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/screen/CreateGameScreen.java
@@ -7,12 +7,15 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public class CreateGameScreen extends VBox {
 
 	private static final String FONT_PATH = "/ThaleahFat.ttf";
+	private static final String BOSS_GIF_PATH = "/images/boss/boss.gif";
 	private static final double TITLE_FONT_SIZE = 32;
 	private static final double BUTTON_FONT_SIZE = 26;
 
@@ -39,6 +42,8 @@ public class CreateGameScreen extends VBox {
 		title.setStyle("-fx-text-fill: white;");
 		title.setFont(Font.font(fontFamily, TITLE_FONT_SIZE));
 
+		ImageView bossImageView = createBossImageView();
+
 		TextField fileNameField = new TextField();
 		fileNameField.setPromptText("Filnavn (f.eks. slot1)");
 		fileNameField.setMaxWidth(450);
@@ -58,7 +63,25 @@ public class CreateGameScreen extends VBox {
 		backButton.setPrefHeight(55);
 		backButton.setOnAction(e -> handler.onBack());
 
-		getChildren().addAll(title, fileNameField, startButton, backButton);
+		if (bossImageView != null) {
+			getChildren().addAll(title, bossImageView, fileNameField, startButton, backButton);
+		} else {
+			getChildren().addAll(title, fileNameField, startButton, backButton);
+		}
+	}
+
+	private ImageView createBossImageView() {
+		InputStream gifStream = getClass().getResourceAsStream(BOSS_GIF_PATH);
+		if (gifStream == null) {
+			return null;
+		}
+
+		Image bossImage = new Image(gifStream);
+		ImageView bossImageView = new ImageView(bossImage);
+		bossImageView.setPreserveRatio(true);
+		bossImageView.setFitHeight(180);
+		bossImageView.setSmooth(true);
+		return bossImageView;
 	}
 
 	private String loadFontFamily() {
