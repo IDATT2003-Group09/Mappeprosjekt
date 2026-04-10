77011637cc36e49e4597c33a2d9c702e50d62ee3
davidomm <davidomm@stud.ntnu.no>
2026-04-10 12:30:27 +0200

    class for gamelevels

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/GameLevel.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/base/GameLevel.java
@@ -1,4 +1,22 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.base;
 
-public enum GameLevel {
+import java.math.BigDecimal;
+
+public class GameLevel {
+
+    private final int levelNumber;
+    private final BigDecimal baseRequirment;
+
+    public GameLevel(int levelNumber, BigDecimal baseRequirment) {
+        this.levelNumber = levelNumber;
+        this.baseRequirment = baseRequirment;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public BigDecimal getBaseRequirment() {
+        return baseRequirment;
+    }
 }
