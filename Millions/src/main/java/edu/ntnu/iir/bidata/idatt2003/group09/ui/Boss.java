4000dc96127fd3bbea8efb75cefd70c23922743c
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-12 21:25:43 +0200

    fuction for how long the boss should be talking

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/Boss.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/Boss.java
@@ -1,27 +1,41 @@
 package edu.ntnu.iir.bidata.idatt2003.group09.ui;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import javafx.animation.PauseTransition;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Boss extends VBox {
 
   private static final String IDLE_PATH = "/images/boss/blinking.gif";
   private static final String TALKING_PATH = "/images/boss/talking.gif";
   private static final String HAIR_PATH = "/images/boss/hair.gif";
+  private static final Duration TALKING_FALLBACK_DURATION = Duration.seconds(2);
 
   private final Image idleImage;
   private final Image talkingImage;
   private final Image hairImage;
+  private final Duration talkingCycleDuration;
   private final ImageView imageView;
   private final ChatBubble chatBubble;
+  private final PauseTransition talkingToIdleTransition;
 
   public Boss(String initialText, String fontFamily, double imageSize) {
     this.idleImage = loadImage(IDLE_PATH, imageSize);
     this.talkingImage = loadImage(TALKING_PATH, imageSize);
     this.hairImage = loadImage(HAIR_PATH, imageSize);
+    this.talkingCycleDuration = loadGifCycleDuration(TALKING_PATH);
 
     this.imageView = new ImageView(idleImage);
     this.imageView.setPreserveRatio(true);
@@ -29,6 +43,8 @@ public class Boss extends VBox {
     this.imageView.setCache(false);
 
     this.chatBubble = new ChatBubble(initialText, fontFamily);
+  this.talkingToIdleTransition = new PauseTransition(talkingCycleDuration.multiply(2));
+  this.talkingToIdleTransition.setOnFinished(event -> setIdle());
 
     setAlignment(Pos.TOP_LEFT);
     setSpacing(10);
@@ -42,6 +58,74 @@ public class Boss extends VBox {
       return null;
     }
     return new Image(imageStream, imageSize, imageSize, true, false);
+  }
+
+  private Duration loadGifCycleDuration(String gifPath) {
+    try (InputStream inputStream = getClass().getResourceAsStream(gifPath)) {
+      if (inputStream == null) {
+        return TALKING_FALLBACK_DURATION;
+      }
+
+      Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("gif");
+      if (!readers.hasNext()) {
+        return TALKING_FALLBACK_DURATION;
+      }
+
+      ImageReader reader = readers.next();
+      try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
+        reader.setInput(imageInputStream, false);
+        int frameCount = reader.getNumImages(true);
+        long totalDelayMs = 0;
+
+        for (int frameIndex = 0; frameIndex < frameCount; frameIndex++) {
+          Node root = reader.getImageMetadata(frameIndex).getAsTree("javax_imageio_gif_image_1.0");
+          totalDelayMs += readFrameDelayMs(root);
+        }
+
+        if (totalDelayMs > 0) {
+          return Duration.millis(totalDelayMs);
+        }
+      } finally {
+        reader.dispose();
+      }
+    } catch (IOException ignored) {
+      return TALKING_FALLBACK_DURATION;
+    }
+
+    return TALKING_FALLBACK_DURATION;
+  }
+
+  private long readFrameDelayMs(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    NodeList children = root.getChildNodes();
+    for (int childIndex = 0; childIndex < children.getLength(); childIndex++) {
+      Node child = children.item(childIndex);
+      if (!"GraphicControlExtension".equals(child.getNodeName())) {
+        continue;
+      }
+
+      NamedNodeMap attributes = child.getAttributes();
+      if (attributes == null) {
+        return 0;
+      }
+
+      Node delayTimeNode = attributes.getNamedItem("delayTime");
+      if (delayTimeNode == null) {
+        return 0;
+      }
+
+      try {
+        int hundredths = Integer.parseInt(delayTimeNode.getNodeValue());
+        return hundredths * 10L;
+      } catch (NumberFormatException ignored) {
+        return 0;
+      }
+    }
+
+    return 0;
   }
 
   public void setIdle() {
@@ -65,6 +149,8 @@ public class Boss extends VBox {
   public void updateTalkingBubble(String text) {
     setTalking();
     chatBubble.setText(text);
+    talkingToIdleTransition.stop();
+    talkingToIdleTransition.playFromStart();
   }
 
   public ChatBubble getChatBubble() {
