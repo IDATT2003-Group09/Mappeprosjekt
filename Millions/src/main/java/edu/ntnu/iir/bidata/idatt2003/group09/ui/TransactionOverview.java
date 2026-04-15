6a6ed2ae5afd671179f1a12297c049c66e3b0e7b
Mats Vestvik <mats.o.vestvik@gmail.com>
2026-04-15 13:43:18 +0200

    styled to same colors as game

--- Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/TransactionOverview.java
+++ Millions/src/main/java/edu/ntnu/iir/bidata/idatt2003/group09/ui/TransactionOverview.java
@@ -19,24 +19,29 @@ public class TransactionOverview extends StackPane {
 
 		VBox box = new VBox(12);
 		box.setPadding(new Insets(32));
-		box.setStyle("-fx-background-color: white; -fx-background-radius: 12; -fx-effect: dropshadow(gaussian, #222, 16, 0.2, 0, 4);");
+		box.getStyleClass().add("transaction-overview-box");
 		box.setMaxWidth(340);
 		box.setMinWidth(260);
 		box.setAlignment(javafx.geometry.Pos.CENTER);
 
 		Label actionLabel = new Label(action + " " + quantity.toPlainString() + " x " + stockSymbol);
-		actionLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+		actionLabel.getStyleClass().add("transaction-overview-action");
 		Label priceLabel = new Label("Price per share: " + price);
+		priceLabel.getStyleClass().add("transaction-overview-label");
 		Label commissionLabel = new Label("Commission (fee): " + commission);
+		commissionLabel.getStyleClass().add("transaction-overview-label");
 		Label taxLabel = new Label("Tax: " + tax);
+		taxLabel.getStyleClass().add("transaction-overview-label");
 		Label totalLabel = new Label("Total: " + total);
-		totalLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+		totalLabel.getStyleClass().add("transaction-overview-total");
 
 		Button confirmButton = new Button("Confirm");
+		confirmButton.getStyleClass().add("transaction-overview-confirm");
 		confirmButton.setOnAction(e -> onConfirm.run());
 		confirmButton.setDefaultButton(true);
 
 		Button closeButton = new Button("Cancel");
+		closeButton.getStyleClass().add("transaction-overview-cancel");
 		closeButton.setOnAction(e -> {
 			// Just remove overlay, do not confirm
 			StackPane parent = (StackPane) getParent();
@@ -45,6 +50,7 @@ public class TransactionOverview extends StackPane {
 
 		HBox buttonBox = new HBox(10, confirmButton, closeButton);
 		buttonBox.setAlignment(javafx.geometry.Pos.CENTER);
+		buttonBox.getStyleClass().add("transaction-overview-buttons");
 
 		box.getChildren().addAll(actionLabel, priceLabel, commissionLabel, taxLabel, totalLabel, buttonBox);
 
