b0db495182f529ef1558ff26b870a8a07eadb4d6
mats orpia vestvik <mats.o.vestvik@gmail.com>
2026-05-13 13:50:09 +0200

    removed random extra folder

--- Millions/src/test/java/edu/ntnu/iir/bidata/idatt2003/group09/model/ExchangeTest.java
+++ Millions/src/test/java/edu/ntnu/iir/bidata/idatt2003/group09/model/ExchangeTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.iir.bidata.idatt2003.group09.model.calculator;
+package edu.ntnu.iir.bidata.idatt2003.group09.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
